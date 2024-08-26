78101efaa4634ab851770463309dee51d290992d
HanunagreZ <hanunagrez@yandex.ru>
2024-08-26 13:05:47 +0300

    feat: add _redirects

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/cooking-lp/',
+  build: {
+    outDir: 'dist',
+    publicPath: '/cooking-lp/',
+  },
 });
